62309dca643be153d56a0b90ef09a28d7cbc9a4b
ramirezra <robinson@camrpc.com>
2017-01-27 17:53:23 -0500

    CSV file

--- section02/01_hands_on/04/main.go
+++ section02/01_hands_on/04/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/csv"
-	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -15,7 +14,8 @@ func init() {
 	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
 }
 
-type record struct {
+// Post is Eported type
+type Post struct {
 	Date     string
 	Open     float64
 	High     float64
@@ -25,55 +25,60 @@ type record struct {
 	AdjClose float64
 }
 
-type records []record
-
-func makeRecord(row []string) record {
-	open, _ := strconv.ParseFloat(row[1], 64)
-	high, _ := strconv.ParseFloat(row[2], 64)
-	low, _ := strconv.ParseFloat(row[3], 64)
-	close, _ := strconv.ParseFloat(row[4], 64)
-	volume, _ := strconv.ParseFloat(row[5], 64)
-	// adjusted, _ := strconv.ParseFloat(row[6], 64)
-
-	return record{
-		Date:   row[0],
-		Open:   open,
-		High:   high,
-		Low:    low,
-		Close:  close,
-		Volume: volume,
-		// Adjusted: adjusted,
-	}
-}
+//
+// func makeRecord(row []string) record {
+// 	open, _ := strconv.ParseFloat(row[1], 64)
+// 	high, _ := strconv.ParseFloat(row[2], 64)
+// 	low, _ := strconv.ParseFloat(row[3], 64)
+// 	close, _ := strconv.ParseFloat(row[4], 64)
+// 	volume, _ := strconv.ParseFloat(row[5], 64)
+// 	// adjusted, _ := strconv.ParseFloat(row[6], 64)
+//
+// 	return record{
+// 		Date:   row[0],
+// 		Open:   open,
+// 		High:   high,
+// 		Low:    low,
+// 		Close:  close,
+// 		Volume: volume,
+// 		// Adjusted: adjusted,
+// 	}
+// }
 
 func main() {
-	data, err := os.Open("table.csv")
+	file, err := os.Open("table.csv")
 	if err != nil {
 		log.Fatalln(err)
 		return
 	}
-	defer data.Close()
+	defer file.Close()
 
 	// var table []string
 
-	for {
-		record, err := csv.NewReader(data).ReadAll()
-		if err != nil {
-			log.Fatalln(err)
-		}
-		for i, row := range record {
-			if i == 0 {
-				continue
-			}
-			fmt.Println(i, row)
+	reader := csv.NewReader(file)
 
-			// record := makeRecord(row)
-		}
+	reader.FieldsPerRecord = -1
+
+	record, err := reader.ReadAll()
+	if err != nil {
+		log.Fatalln(err)
 	}
 
-	// err := tpl.Execute(os.Stdout, record)
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
+	var posts []Post
 
+	for _, item := range record {
+		open, _ := strconv.ParseFloat(item[1], 64)
+		// high, _ := strconv.ParseFloat(record[2], 64)
+		// low, _ := strconv.ParseFloat(record[3], 64)
+		// close, _ := strconv.ParseFloat(record[4], 64)
+		// volume, _ := strconv.ParseFloat(record[5], 64)
+
+		post := Post{Date: item[0], Open: open}
+		posts = append(posts, post)
+	}
+
+	error := tpl.Execute(os.Stdout, posts)
+	if err != nil {
+		log.Fatalln(error)
+	}
 }
