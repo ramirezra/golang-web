242548e1e8205e59b6deffccba7aff1d1eaad78a
ramirezra <robinson@camrpc.com>
2017-01-31 10:50:46 -0500

    Modify

--- section02/01_hands_on/04/main.go
+++ section02/01_hands_on/04/main.go
@@ -21,9 +21,29 @@ type Post struct {
 	High     float64
 	Low      float64
 	Close    float64
-	Volume   int64
+	Volume   float64
 	AdjClose float64
 }
+
+//
+// func makeRecord(row []string) record {
+// 	open, _ := strconv.ParseFloat(row[1], 64)
+// 	high, _ := strconv.ParseFloat(row[2], 64)
+// 	low, _ := strconv.ParseFloat(row[3], 64)
+// 	close, _ := strconv.ParseFloat(row[4], 64)
+// 	volume, _ := strconv.ParseFloat(row[5], 64)
+// 	// adjusted, _ := strconv.ParseFloat(row[6], 64)
+//
+// 	return record{
+// 		Date:   row[0],
+// 		Open:   open,
+// 		High:   high,
+// 		Low:    low,
+// 		Close:  close,
+// 		Volume: volume,
+// 		// Adjusted: adjusted,
+// 	}
+// }
 
 func main() {
 	file, err := os.Open("table.csv")
@@ -46,28 +66,18 @@ func main() {
 
 	var posts []Post
 
-	for i, item := range record {
+	for _, item := range record {
 		open, _ := strconv.ParseFloat(item[1], 64)
-		high, _ := strconv.ParseFloat(item[2], 64)
-		low, _ := strconv.ParseFloat(item[3], 64)
-		close, _ := strconv.ParseFloat(item[4], 64)
-		volume, _ := strconv.ParseInt(item[5], 0, 0)
+		// high, _ := strconv.ParseFloat(record[2], 64)
+		// low, _ := strconv.ParseFloat(record[3], 64)
+		// close, _ := strconv.ParseFloat(record[4], 64)
+		// volume, _ := strconv.ParseFloat(record[5], 64)
 
-		if i == 0 {
-			continue
-		}
-
-		post := Post{Date: item[0], Open: open, High: high, Low: low, Close: close, Volume: volume}
+		post := Post{Date: item[0], Open: open}
 		posts = append(posts, post)
 	}
 
-	newfile, err := os.Create("index.html")
-	if err != nil {
-		log.Println("error creating file", err)
-	}
-	defer newfile.Close()
-
-	error := tpl.Execute(newfile, posts)
+	error := tpl.Execute(os.Stdout, posts)
 	if err != nil {
 		log.Fatalln(error)
 	}
