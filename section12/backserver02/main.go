065ab4f0239441b41ae408f18a79e0e0ae25fab0
Robinson Ramirez <robinson@camrpc.com>
2017-02-15 16:50:04 -0500

    Section 12: HTTP Load Balancer

---
+++ section12/backserver02/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+// var db *sql.DB
+// var err error
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/ping", ping)
+	http.HandleFunc("/instance", instance)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":80", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello from CAM Server 2")
+}
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "OK")
+}
+
+func instance(w http.ResponseWriter, r *http.Request) {
+	data := struct {
+		Method        string
+		URL           *url.URL
+		Submissions   url.Values
+		Header        http.Header
+		Host          string
+		ContentLength int64
+	}{
+		r.Method,
+		r.URL,
+		r.Form,
+		r.Header,
+		r.Host,
+		r.ContentLength,
+	}
+	tpl.ExecuteTemplate(w, "index.gohtml", data)
+}
