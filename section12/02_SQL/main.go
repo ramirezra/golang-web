9477170b67898d6900ff19c5b14a7a691b54fbfa
Robinson Ramirez <robinson@camrpc.com>
2017-02-16 15:16:43 -0500

    Section12 - SQL Server Balanced

--- section12/02_SQL/main.go
+++ section12/02_SQL/main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	_ "github.com/go-sql-driver/mysql"
 	"io"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var db *sql.DB
 var err error
 
 func main() {
-	db, err = sql.Open("mysql", "awsuser:mypassword@tcp(mydbinstance.cakwl95bxza0.us-west-1.rds.amazonaws.com:3306)/test02?charset=utf8")
+	db, err = sql.Open("mysql", "awsuser:mypassword@tcp(mydbinstance.cakwl95bxza0.us-west-1.rds.amazonaws.com:3306)/ping?charset=utf8")
 	check(err)
 	defer db.Close()
 
