5987e78bf9411c4d5d74f31da617f8fb712dbc63
ramirezra <robinson@camrpc.com>
2017-02-16 23:37:31 -0500

    Section 13

--- section13-photoblog/main.go
+++ section13-photoblog/main.go
@@ -3,17 +3,53 @@ package main
 import (
 	"html/template"
 	"net/http"
+
+	uuid "github.com/satori/go.uuid"
 )
+
+var tpl *template.Template
+var dbUsers = map[string]user{}      // userID, user
+var dbSessions = map[string]string{} // session ID, user ID
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
 
 func main() {
 	http.HandleFunc("/", index)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
 
 	http.ListenAndServe(":8080", nil)
 }
 
-var tpl *template.Template
-
 func index(w http.ResponseWriter, r *http.Request) {
 	u := getUser(w, r)
 	tpl.ExecuteTemplate(w, "index.gohtml", u)
+}
+
+// Session and Cookie Setting
+type user struct {
+	UserName string
+	Password []byte
+	First    string
+	Last     string
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) user {
+	c, err := r.Cookie("session")
+	if err != nil {
+		sID := uuid.NewV4()
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+	}
+	http.SetCookie(w, c)
+
+	// Get user, if already exists.
+	var user user
+	if un, ok := dbSessions[c.Value]; ok {
+		user = dbUsers[un]
+	}
+	return user
 }
