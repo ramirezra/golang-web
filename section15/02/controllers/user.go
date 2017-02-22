1474bbe4f1a90749135b74dabdd158a3122bb4d4
ramirezra <robinson@camrpc.com>
2017-02-21 23:08:42 -0500

    Section 15 - starting 02

---
+++ section15/02/controllers/user.go
@@ -0,0 +1,71 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/ramirezra/golang-web/section15/01/models"
+	uuid "github.com/satori/go.uuid"
+)
+
+// UserController exported
+type UserController struct {
+	session map[string]models.User
+}
+
+// NewUserController exported
+func NewUserController(m map[string]models.User) *UserController {
+	return &UserController{m}
+}
+
+// GetUser exported
+func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	// Grab id
+	id := p.ByName("id")
+
+	// Fetch user
+	u := uc.session[id]
+
+	// Marshal provided interface into JSON structure
+	uj, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK) // 200
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+// CreateUser exported
+func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	u := models.User{}
+
+	json.NewDecoder(r.Body).Decode(&u)
+
+	// create ID
+	u.Id = uuid.NewV4().String()
+
+	// store the user
+	uc.session[u.Id] = u
+	uj, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated) // 201
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+// DeleteUser exported
+func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+
+	// Delete user
+	delete(uc.session, id)
+	w.WriteHeader(http.StatusOK) // 200
+	fmt.Fprint(w, "Deleted user", id, "\n")
+}
